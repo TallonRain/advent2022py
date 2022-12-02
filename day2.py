4e703e978cd2628ea2b095e6cd1f507c45af1f08
Kyle Worthington <tallonrain@tallonra.in>
2022-12-01 23:51:03 -0800

    day2, amended

--- day2.py
+++ day2.py
@@ -8,32 +8,29 @@ with open("inputs/day2") as f:
 letterVal = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
 losePair = {'A': 'Z', 'B': 'X', 'C': 'Y'}
 winPair = {'A': 'Y', 'B': 'Z', 'C': 'X'}
-totalScore = 0
+partOne = 0
 partTwo = 0
+
 for i in lines:
     divided = i.split(' ')
     left = divided[0].strip()
     right = divided[1].strip()
-    score = letterVal.get(right)
-    tieLeft = letterVal.get(left)
 
-    if tieLeft == score:
-        totalScore += 3 + score
-    elif (left == 'A' and right == 'Z') or (left == 'B' and right == 'X') or (left == 'C' and right == 'Y'):
-        totalScore += 0 + score
-    else:
-        totalScore += 6 + score
+    if letterVal[left] == letterVal[right]:  # tie
+        partOne += 3 + letterVal[right]
+    elif losePair[left] == right:  # lose
+        partOne += 0 + letterVal[right]
+    else:  # win
+        partOne += 6 + letterVal[right]
 
     if right == 'X':  # must lose
-        loseValue = letterVal.get(losePair.get(left))
-        partTwo += 0 + loseValue
+        partTwo += 0 + letterVal[losePair[left]]
     elif right == 'Y':  # must tie
-        partTwo += 3 + tieLeft
+        partTwo += 3 + letterVal[left]
     elif right == 'Z':  # must win
-        winValue = letterVal.get(winPair.get(left))
-        partTwo += 6 + winValue
+        partTwo += 6 + letterVal[winPair[left]]
 
-print(totalScore)
+print(partOne)
 print(partTwo)
 
 # well that's embarrassingly clunky
